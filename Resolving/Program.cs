7f75f12c7474851285487f8e57b2d4b6a6a9d94c
LkDrm <larik115@gmail.com>
2024-08-18 14:39:07 +0200

    First commit - Make Resolving project

---
+++ Resolving/Program.cs
@@ -0,0 +1,8 @@
+﻿#region Start
+Console.Title = "Codes result";
+var legendDefault = ConsoleColor.Green;
+var legendRedResult = ConsoleColor.Red;
+Console.ForegroundColor = legendDefault;
+Console.WriteLine("Here u can see the resolved tasks!");
+#endregion
+
