9f71f15213db6d61132efbdccd4abfb41ebc4fb0
LkDrm <larik115@gmail.com>
2024-08-24 14:22:07 +0200

    Resolve task about --> "Merge Sorted Array"
    Signed-off-by: LkDrm <larik115@gmail.com>

--- Resolving/Program.cs
+++ Resolving/Program.cs
@@ -1,8 +1,15 @@
-﻿#region Start
+﻿using Resolving;
+
+#region Start
 Console.Title = "Codes result";
 var legendDefault = ConsoleColor.Green;
-var legendRedResult = ConsoleColor.Red;
 Console.ForegroundColor = legendDefault;
 Console.WriteLine("Here u can see the resolved tasks!");
 #endregion
 
+var mergeSortedArrayTask1 = new MergeSortedArray().ResolvingTask(firstArray: [1, 2, 3, 0, 0, 0], m: 3, secondArray: [2, 5, 6], n: 3);
+Console.WriteLine("First [{0}]", string.Join(",", mergeSortedArrayTask1));
+var mergeSortedArrayTask2 = new MergeSortedArray().ResolvingTask(firstArray: [1], m: 1, secondArray: [], n: 0);
+Console.WriteLine("Second [{0}]", string.Join(",", mergeSortedArrayTask2));
+var mergeSortedArrayTask3 = new MergeSortedArray().ResolvingTask(firstArray: [0], m: 0, secondArray: [1], n: 1);
+Console.WriteLine("Third [{0}]", string.Join(",", mergeSortedArrayTask3));
---
+++ Resolving/Task1.cs
@@ -0,0 +1,35 @@
+﻿namespace Resolving
+{
+    /// <summary>
+    /// Merge Sorted Array
+    /// </summary>
+    public class MergeSortedArray
+    {
+        /// <summary>
+        /// Resolving task about sorting array
+        /// </summary>
+        /// <returns>Sorted array</returns>
+        public int[] ResolvingTask(int[] firstArray, int m, int[] secondArray, int n)
+        {
+            int firstArrayLenght = m - 1;
+            int secondArrayLenght = n - 1;
+            int arrayLength = (m + n) - 1;
+            while (secondArrayLenght >= 0)
+            {
+                if(firstArrayLenght >=0 && firstArray[firstArrayLenght] > secondArray[secondArrayLenght])
+                {
+                    firstArray[arrayLength] = firstArray[firstArrayLenght];
+                    arrayLength--;
+                    firstArrayLenght--;
+                }
+                else
+                {   
+                    firstArray[arrayLength] = secondArray[secondArrayLenght];
+                    arrayLength--;
+                    secondArrayLenght--;
+                }
+            }
+            return firstArray;
+        }
+    }
+}
